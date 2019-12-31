83b84501b161a6c677db2145bb6f272a5522c51f
Alberto <40391280+alberto342@users.noreply.github.com>
2019-12-31 21:17:18 +0200

    Update ShowEmptyView.java

--- app/src/main/java/com/example/movieapp/movie_list/ShowEmptyView.java
+++ app/src/main/java/com/example/movieapp/movie_list/ShowEmptyView.java
@@ -1,9 +1,4 @@
-/**
- * @file ShowEmptyView.java
- * @brief This is an interface to hide and show empty view on recycler view.
- * @author Shrikant
- * @date 16/04/2018
- */
+
 package com.example.movieapp.movie_list;
 
 public interface ShowEmptyView {
